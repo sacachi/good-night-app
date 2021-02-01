420afc1fa1222578875d52b20bf156d3a9e85717
Sach Nguyen Van <sacachi@gmail.com>
2021-02-02 01:05:23 +0700

    add cors, page pgination with kaminari, rails router

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-  has_many :sleep_trackers, dependent: :destroy
+  has_many :sleep_trackers, -> { order(created_at: :desc).limit(100) }
   has_many :following_users, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy
   has_many :followed_users, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :followings, through: :following_users
